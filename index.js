996592d660696c4e1288fcb5ddbbe79a2795c867
zachrycaleb <zachry.caleb@gmail.com>
2016-11-28 13:43:44 -0600

    yob function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+function yearOfBirth (age) {
+  return 2016 - age
+}	
 function whoAmI(name, age) {
-  var yearOfBirth = 2016 - age;
+  var yob = yearOfBirth(age)
   console.log('Hi my name is ' + name + ' and I\'m ' + age + ' years old');
-  console.log('I was born on ' + yearOfBirth);
+  console.log('I was born on ' + yob);
 }
 whoAmI('Roberto', 36);
