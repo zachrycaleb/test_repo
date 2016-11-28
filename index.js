4f95a5f39282f4a926779ee2a64be500750a931a
sgtquezada <sgtquezada@gmail.com>
2016-11-28 11:24:09 -0800

    added yearOfBirth variable

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function whoAmI(name, age) {
+  var yearOfBirth = 2016 - age;
   console.log('Hi my name is ' + name + ' and I\'m ' + age + ' years old');
+  console.log('I was born on ' + yearOfBirth);
 }
 whoAmI('Roberto', 36);
